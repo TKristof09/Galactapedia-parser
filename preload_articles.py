f8d9a55bbf39458067a7ff8dc6a7ff3f6a883372
Kristof Toth <toth.kristof111@gmail.com>
2023-11-18 18:34:23 +0100

    article preloader script

--- preload_articles.py
+++ preload_articles.py
@@ -1,14 +1,18 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import threading
 from time import sleep
 import random
 
 import json
+
+if len(sys.argv) > 1:
+    force_refresh = sys.argv[1] == "--force-refresh"
+else:
+    force_refresh = False
 
 LINKS_FILENAME = "links.json"
 ARTICLES_FILENAME = "articles.json"
@@ -26,55 +30,45 @@ except FileNotFoundError:
         f.write(json.dumps({}))
 
 
-def split(d, chunks):
-    chunk_len = len(d) // chunks + 1
-    chunks_list = []
-    count = 0
-    for title, link in d.items():
-        if count % chunk_len == 0:
-            chunks_list.append({})
-        chunks_list[-1][title] = link
-        count += 1
-    return chunks_list
-
-stop = False
+errored_articles = 0
 def parse_article(links, id):
+    global errored_articles
+    global articles
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_window_size(1920,500)
+    count = 0
     for title, link in links.items():
-        if stop:
-            break
-        if title in articles:
+        if title in articles and not force_refresh:
             continue
+        if count % 10 == 0:
+            with open(ARTICLES_FILENAME + str(count // 10), 'w') as f:
+                print(f"Saving articles checkpoint {str(count // 10)}...")
+                json.dump(articles, f, indent=4)
         try:
             driver.get(link)
-            print(f"Thread #{id}: Loading {title} from {link}")
-            sleep(random.uniform(1.0, 7.0)) # sleep to avoid making too many requests to the website
-            article = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//section[@class='p-article__content']//div[@class='c-markdown-content']")))
-            text = article.get_property("children")[0].text
+            sleep(random.uniform(20.0, 35.0)) # sleep to avoid making too many requests to the website
+            paragraphs = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//section[@class='p-article__content']//div[@class='c-markdown-content']/p")))
+            text = ""
+            for p in paragraphs:
+                text += p.text + "\n"
+            print(f"Thread #{id}: Loaded {title} from {link}")
             articles[title] = text
+            count += 1
         except Exception as e:
-            break
+            print(f"Thread #{id}: Error {repr(e)} loading {title} from {link}. Skipping...")
+            errored_articles += 1
+            continue
     driver.quit()
 
-threads = []
-for l in split(links, 2):
-    th = threading.Thread(target=parse_article, args=(l,len(threads)))
-    th.start() # could `time.sleep` between 'clicks' to see whats'up without headless option
-    threads.append(th)
 try:
-    for th in threads:
-        th.join() # Main thread wait for threads finish
+    parse_article(links, 0)
 except KeyboardInterrupt:
     print("Stopping...")
-    stop = True
-    for th in threads:
-        th.join() # Main thread wait for threads finish
 
-
-
-import json
+if errored_articles > 0:
+    print(f"{errored_articles} articles couldn't be loaded (due to timeouts most likely), try running the script again.")
 with open(ARTICLES_FILENAME, 'w') as f:
-    json.dump(articles, f)
+    print("Saving articles...")
+    json.dump(articles, f, indent=4)
